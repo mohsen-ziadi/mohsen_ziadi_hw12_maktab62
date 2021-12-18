9429c7900e7e80b987f8190246b15b2cab8c17a9
mohsen-ziadi <mohsen@pop-os.localdomain>
2021-12-18 18:31:01 +0330

    updat

--- website/app.js
+++ website/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const router = require('./router/home.js');
 const path = require('path');
-const port = 5000;
+const port = 4000;
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
--- website/router/home.js
+++ website/router/home.js
@@ -2,32 +2,31 @@ const express = require('express');
 const router = express.Router();
 const path = require("path");
 const producteJSON = require("../database/products.json");
+let i;
+
 
 router.get("/", (req, res) => {
-    res.render("home", { data: producteJSON, i: 0 });
-    // res.send("hello")
+    res.render("home", { data: producteJSON, i })
+        // res.send("hello")
 })
 
 router.get("/contact", (req, res) => {
-    res.render("contact", { data: producteJSON });
+    res.render("contact", { data: producteJSON, i });
     // res.send("hello")
 })
 
 router.get("/about", (req, res) => {
-    res.render("about", { data: producteJSON });
+    res.render("about", { data: producteJSON, i });
     // res.send("hello")
 })
 
-router.get("/producteJSON[]", (req, res) => {
-    res.render("about", { data: producteJSON });
+router.get("/product/:id", (req, res) => {
+    res.render("product", { data: producteJSON, i: req.params.id });
     // res.send("hello")
 })
 
 
-router.get('/:id', (req, res) => {
-    res.render(path.join(__dirname, `../views/${req.params.file}.html`))
 
-})
 
 
 module.exports = router;
